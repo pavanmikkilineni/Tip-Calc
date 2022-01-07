866cdc0c42a4909f3df39798e81f2bf62f0ca383
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2022-01-07 05:20:28 -0800

    Initial Commit

---
+++ Tip-Calc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip-Calc
+//
+//  Created by Pavan Mikkilineni on 07/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tip-Calc/Tip_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalcApp.swift
+//  Tip-Calc
+//
+//  Created by Pavan Mikkilineni on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
